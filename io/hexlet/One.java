9ad6f47c843fedb7a57bc1ef7613f18330b7ba62
Andrey Eryomenko <ean3ena@yandex.ru>
2024-06-13 00:21:57 +0300

    update  io/hexlet/One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        //Two.greeting();
+        String reverseWord = StringUtils.reverse("Hexlet");
+        System.out.println(reverseWord);
     }
 }
